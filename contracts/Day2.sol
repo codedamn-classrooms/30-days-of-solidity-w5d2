3be668e16369d099f0c0e019b0b03b1a89956d22
kshitijofficial <52281964+kshitijofficial@users.noreply.github.com>
2022-05-02 16:00:09 +0530

    Update Day2.sol

--- contracts/Day2.sol
+++ contracts/Day2.sol
@@ -2,14 +2,5 @@
 pragma solidity >=0.5.0 <0.9.0;
 
 contract Day2 {
-    address owner;
 
-    constructor() {
-        owner = msg.sender;
-    }
-
-    function returnOwner() public returns (address) {
-        require(owner == msg.sender, "You are not the owner");
-        return owner;
-    }
 }
